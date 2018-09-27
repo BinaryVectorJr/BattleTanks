06c3ba54dcec67e0316be79d1b8adb6ea55287d0
Angshuman Mazumdar <binaryvectorjr@gmail.com>
2018-09-27 10:47:37 +0530

    Project Created for first time
    Moved the .gitignore to inside project folder, and created a new C++ project

---
+++ BattleTanks/Source/BattleTanks/BattleTanksGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKS_API ABattleTanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
